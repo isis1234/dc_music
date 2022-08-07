d3ad9ed99ad07c31b78500fd1e43d6bf7a64b5ff
isis1234 <isistang1234@gmail.com>
2022-08-07 21:52:34 +0800

    ping listener completed

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 require("dotenv").config()
-const ytdl = require('ytdl-core')
-const Discord = require('discord.js')
+const { Client, GatewayIntentBits } = require('discord.js')
 const Music = require("./Music")
 
 const TOKEN = process.env.BOT_TOKEN
 const BOT_CLIENT_ID = process.env.BOT_CLIENT_ID
 
-const { Client, GatewayIntentBits } = require('discord.js');
 const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
 bot.login(TOKEN)
 
 // Init Command
-require("./init")(bot, Discord.Routes, Discord.REST, BOT_CLIENT_ID, TOKEN)
+require("./init")(bot, BOT_CLIENT_ID, TOKEN)
 
-bot.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!')
-  }
-});
+// Command /ping listener
+require("./ping")(bot, BOT_CLIENT_ID, TOKEN)
--- init.js
+++ init.js
@@ -1,24 +1,42 @@
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-]
+const { SlashCommandBuilder, Routes, REST } = require('discord.js')
 
-module.exports = (bot, Routes, REST, BOT_CLIENT_ID, BOT_TOKEN) => {
+module.exports = (bot, BOT_CLIENT_ID, BOT_TOKEN) => {
   try{
     bot.on("ready", async () => {
-        console.log(`${bot.user.tag} restarted.`)
-        const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
-        
-        console.log('Started refreshing application (/) commands.')
-        const Guilds = bot.guilds.cache.map(guild => guild.id)
-        for(let i=0; i<Guilds.length; i++){
-          await rest.put(Routes.applicationGuildCommands(BOT_CLIENT_ID.toString(), Guilds[i].toString()), { body: commands });
-        }
-        console.log('Successfully reloaded application (/) commands.')
+      console.log(`${bot.user.tag} restarted.`)
+      const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+      
+      console.log('Started refreshing application (/) commands.')
+      const Guilds = bot.guilds.cache.map(guild => guild.id)
+      for(let i=0; i<Guilds.length; i++){
+        await rest.put(Routes.applicationGuildCommands(BOT_CLIENT_ID.toString(), Guilds[i].toString()), { body: command_builder() });
+        // await rest.put(Routes.applicationGuildCommands(BOT_CLIENT_ID.toString(), Guilds[i].toString()), { body: commands });
+      }
+      console.log('Successfully reloaded application (/) commands.')
     })
   }catch(error){
     console.error(error)
   }
+}
+
+function command_builder(){
+ let command_list = []
+  
+  // /ping
+  command_list.push(new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!')
+  )
+  
+  // /music
+  command_list.push(new SlashCommandBuilder()
+    .setName('music')
+    .setDescription('Music bot')
+    .addStringOption(option =>
+      option.setName('url')
+        .setDescription('YouTube url')
+        .setRequired(true))
+  )
+
+  return command_list
 }
